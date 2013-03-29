8da4d6a00d4f449305b2ad35b181153954f46609
Vicky Enalen <Peelings@Vickys-Mac-Pro.local>
2013-03-29 12:53:45 -0400

    Changed

---
+++ MedicalApp/UserViewController.h
@@ -0,0 +1,14 @@
+//
+//  UserViewController.h
+//  MedicalApp
+//
+//  Created by Vicky Enalen on 2013-03-29.
+//  Copyright (c) 2013 Haptic5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserViewController : UIViewController
+<UITableViewDelegate, UITableViewDataSource>
+
+@end
