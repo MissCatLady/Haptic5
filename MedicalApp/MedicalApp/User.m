e31a25653f1603e43874a8b3d99cb7a0eba2714b
Segfaulter <me@taylordickson.ca>
2013-03-31 13:38:58 -0400

    Partially Implemented User Class
    Added Settings Icon for Lia

---
+++ MedicalApp/MedicalApp/User.m
@@ -0,0 +1,25 @@
+//
+//  User.m
+//  MedicalApp
+//
+//  Created by Taylor Dickson on 2013-03-31.
+//  Copyright (c) 2013 Haptic5. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+- (void) setFirstname: (NSString*)newFirstname{
+        
+}
+- (void) setLastname: (NSString*)newLastname{
+    
+}
+//Get Methods
+- (NSString*) firstname{
+    return firstname;
+}
+- (NSString*) lastname{
+    return lastname;
+}
+@end
