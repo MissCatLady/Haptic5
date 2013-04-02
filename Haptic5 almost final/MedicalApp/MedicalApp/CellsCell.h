645bc3e1fb72ee7290f1698b3a987d7a769d5972
Segfaulter <me@taylordickson.ca>
2013-04-02 02:15:55 -0400

    Wilson's stuff

---
+++ Haptic5 almost final/MedicalApp/MedicalApp/CellsCell.h
@@ -0,0 +1,13 @@
+//
+//  CellsCell.h
+//  MedicalApp
+//
+//  Created by Wilson Sun on 2013-04-01.
+//  Copyright (c) 2013 Haptic5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellsCell : UITableViewCell
+
+@end
---
+++ Haptic5 almost final/MedicalApp/MedicalApp/MainViewController.h
@@ -0,0 +1,18 @@
+//
+//  PasswordProtectionViewController.h
+//  MedicalApp
+//
+//  Created by Vicky Enalen on 2013-03-25.
+//  Copyright (c) 2013 Haptic5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController {
+    
+}
+
+-(IBAction)passwordAlert;
+-(IBAction)exportAlert;
+
+@end
